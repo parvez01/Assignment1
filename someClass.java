163bb04b0e492f846e128e3f2ac5ba6ed22484f6
Mohammed Parvez <miraz_01@yahoo.co.in>
2016-04-03 13:21:53 -0700

    initial

---
+++ someClass.java
@@ -0,0 +1,10 @@
+public class TheMotivator {
+	public void functionOne() {
+		syso("I couldn't repair your brakes, so I made your horn louder")
+	}
+	public static void main(String[] args) {
+		TheMotivator tm = new TheMotivator();
+		tm.feedback(60);
+	}
+}
+
