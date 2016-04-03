8314d02afc89f8033b8ec4eb60d5c1159096f38d
Mohammed Parvez <miraz_01@yahoo.co.in>
2016-04-03 13:40:04 -0700

    Add Multiple events

--- someClass.java
+++ someClass.java
@@ -3,14 +3,24 @@ public class TheMotivator {
 		syso("On the other hand, you have different fingers")
 	}
 
-public void headerupcomingEvents() {
-		syso("Party at Jane's house tomorrow]")
+public void createEvents() {
+		events.add("We're going to a movie on Saturday");
+		events.add("Study session on Sunday - Jim's house");
 	}
 
+
+public void upcomingEvents() {
+		syso("Upcoming Events")
+for (String event : events)	
+			System.out.println(event);
+	
+}
+              
 
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
 		tm.feedback(60);
+                sc.createEvents();
 	}
 }
 
