f8dd34b819a1b20843354853238faffd9f0cb959
Mohammed Parvez <miraz_01@yahoo.co.in>
2016-04-03 13:26:32 -0700

    AddEvents

--- someClass.java
+++ someClass.java
@@ -2,6 +2,12 @@ public class TheMotivator {
 	public void thoughtForTheDay() {
 		syso("I couldn't repair your brakes, so I made your horn louder")
 	}
+
+public void upcomingEvents() {
+		syso("Party at Jane's house tomorrow]")
+	}
+
+
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
 		tm.feedback(60);
